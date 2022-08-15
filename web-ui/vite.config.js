78f5759d1d1c64d262b68fb4c79bee3c68ab3076
Johan Otterud <johan.otterud@gmail.com>
2022-08-15 15:09:38 +0200

    Using vite instead of CRA for sample project

---
+++ web-ui/vite.config.js
@@ -0,0 +1,10 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+});
